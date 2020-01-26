2e4b1fca68d76751943def3fe207eb37f2297301
Kyle <kylestang.ks@gmail.com>
2020-01-25 19:04:02 -0800

    Finished question logic

--- questions.js
+++ questions.js
@@ -1,6 +1,50 @@
-class Question{
-    constructor(question, answer){
+class Question {
+    constructor(question, correct, fake1, fake2, fake3) {
         this.question = question;
-        this.answer = answer;
+        this.correct = correct;
+        this.options = [correct, fake1, fake2, fake3];
+    }
+
+    shuffleOptions() {
+        var currentIndex = this.options.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = this.options[currentIndex];
+            this.options[currentIndex] = this.options[randomIndex];
+            this.options[randomIndex] = temporaryValue;
+        }
     }
 }
+
+class Questions {
+    constructor() {
+        this.questions = [];
+        this.current = 0;
+    }
+
+    getNext(){
+        if(this.questions.length > 0){
+            this.current++;
+            if(this.current >= this.questions.length){
+                this.current = 0;
+            }
+            return this.questions[this.current];
+        }
+        else{
+            return new Question(
+            "This is a sample question that only appears when you have no questions.\
+             Add questions from the start menu",
+            "Correct answer", "Wrong answer", "Still wrong", "Nope");
+        }
+    }
+}
+
+let list = new Questions();
+//list.questions.push(new Question("What colour is the Salish Sea?", "Blue", "Mauve", "Orange", "Four"));
---
+++ showQuestion.js
@@ -0,0 +1,57 @@
+
+function showCorrect(currentQuestion, clicked){
+    if(!answered){
+        for(let i = 0; i < 4; i++){
+            let anOption = p_options[i];
+            if(anOption.innerHTML == currentQuestion.correct){
+                anOption.classList.remove("option");
+                anOption.classList.add("correct");
+            } else if (clicked != currentQuestion.correct) {
+                anOption.classList.remove("option");
+                anOption.classList.add("wrong");
+            }
+            anOption.classList.remove("hover");
+        }
+
+        div_content = document.getElementById("questionContent");
+        nextButton = document.createElement("P");
+        nextButton.innerHTML = "Next";
+        nextButton.className = "normalButton hover noselect";
+        nextButton.id = "nextButton";
+        nextButton.addEventListener("click", loadNextQuestion);
+        div_content.appendChild(nextButton);
+
+        answered = true;
+    }
+}
+
+function loadNextQuestion(){
+    if(document.getElementById("nextButton") != null){
+        document.getElementById("nextButton").remove();
+    }
+
+    answered = false;
+    currentQuestion = list.getNext();
+    currentQuestion.shuffleOptions();
+    
+    p_question.innerHTML = currentQuestion.question;
+    for (let i = 0; i < 4; i++) {
+        let p_currentOption = p_options[i];
+        p_currentOption.classList = "option hover noselect";
+        p_currentOption.innerHTML = currentQuestion.options[i];
+        p_currentOption.addEventListener("click", () =>{
+            showCorrect(currentQuestion, currentQuestion.options[i]);
+        });
+    }
+}
+
+let answered = false;
+let div_answer = document.getElementById("answer");
+let p_question = document.getElementById("question");
+let p_option1 = document.getElementById("option0");
+let p_option2 = document.getElementById("option1");
+let p_option3 = document.getElementById("option2");
+let p_option4 = document.getElementById("option3");
+let p_options = [p_option1, p_option2, p_option3, p_option4];
+
+loadNextQuestion();
